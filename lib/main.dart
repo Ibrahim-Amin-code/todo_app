e710e06f4ffe6b9af2f109feea5a7c82e5fbef01
Ibrahim-Amin-code <ibrahimamin081@gmail.com>
2021-07-23 14:30:41 +0200

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_app/modules/counter/counter_screeen.dart';
 import 'package:todo_app/shared/bloc_observer.dart';
 
 import 'layout/layout_screen.dart';
 
-void main()
-{
+void main() {
   Bloc.observer = MyBlocObserver();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
